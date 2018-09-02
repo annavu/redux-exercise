d6c1364c59e72266065eb5c27068bfd6300eb9e7
annavu <anna.wrzos@gmail.com>
2018-09-02 15:23:57 +0200

    implement Provider component with store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+
+
 import Posts from './components/Posts';
-import Form from './components/Form'
+import Form from './components/Form';
+
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Form/>
-      <Posts/>  
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Form/>
+          <Posts/>  
+        </div>
+      </Provider>
     );
   }
 }
