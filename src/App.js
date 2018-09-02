9f536ff0521a368501e3354a4917be351d7734b1
annavu <anna.wrzos@gmail.com>
2018-09-02 12:23:13 +0200

    update main.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import Posts from './components/Posts';
+import Form from './components/Form'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Form/>
       <Posts/>  
-      
       </div>
     );
   }
