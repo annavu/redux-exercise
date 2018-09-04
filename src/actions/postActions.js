827d9e77df36ec7d9a4645ac1e83c44c25b93e2c
annavu <anna.wrzos@gmail.com>
2018-09-04 16:32:08 +0200

    create new action - create post and specify reducer

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -13,3 +13,19 @@ export function fetchPosts() {
 }
 
 
+export function createPost(postData) {
+  return function(dispatch) {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(postData)
+    })
+    .then(res => res.json())
+    .then(post => dispatch({
+      type: NEW_POST,
+      payload: post 
+    }))
+  }
+}
