dfa47b8987894ab1337c5f7cfc1d542d86c28f11
annavu <anna.wrzos@gmail.com>
2018-09-04 16:34:39 +0200

    update mapStateToProps and add new post to list - component will receive props

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -23,7 +23,11 @@ class Posts extends Component {
     this.props.fetchPosts();
   }
 
-
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.newPost) {
+      this.props.posts.unshift(nextProps.newPost)
+    }
+  }
 
   render() {
 
@@ -45,11 +49,13 @@ class Posts extends Component {
 
 Posts.propTypes = {
   fetchPosts: PropTypes.func.isRequired,
-  posts: PropTypes.array.isRequired
+  posts: PropTypes.array.isRequired,
+  newPost: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-  posts: state.posts.items
+  posts: state.posts.items,
+  newPost: state.posts.item
 });
 
 
