b3a29084bec1e00694abd327126b84c2efd93bf9
annavu <anna.wrzos@gmail.com>
2018-09-04 16:33:10 +0200

    replace fetch with call action

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
 
-export default class Form extends Component {
+class Form extends Component {
 
   constructor() {
     super();
@@ -25,15 +28,19 @@ export default class Form extends Component {
       body: this.state.body
     }
 
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
+//call action
+
+    this.props.createPost(post);
+
+    // fetch('https://jsonplaceholder.typicode.com/posts', {
+    //   method: 'POST',
+    //   headers: {
+    //     'content-type': 'application/json'
+    //   },
+    //   body: JSON.stringify(post)
+    // })
+    // .then(res => res.json())
+    // .then(data => console.log(data))
   }
 
   render() {
@@ -49,3 +56,10 @@ export default class Form extends Component {
     )
   }
 }
+
+Form.propTypes = {
+  createPost: PropTypes.func.isRequired
+}
+
+
+export default connect(null, { createPost })(Form);
